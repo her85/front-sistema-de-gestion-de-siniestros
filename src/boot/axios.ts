cb60e977cb92cbed798c546db4b898443f5fa17b
her85 <herbotto1985@gmail.com>
2026-01-14 15:29:54 -0300

    feat: update axios configuration to use environment variable for base URL

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,12 +1,14 @@
-import { boot } from 'quasar/wrappers'
-import axios from 'axios'
+import { boot } from 'quasar/wrappers';
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:3000/api',
-})
+  baseURL: 'https://api-gestion-de-siniestros.onrender.com',
+  //baseURL: 'http://localhost:3000/api',
+});
 
+console.log({ baseURL: api.defaults.baseURL });
 export default boot(({ app }) => {
-  app.config.globalProperties.$api = api
-})
+  app.config.globalProperties.$api = api;
+});
 
-export { api }
+export { api };
